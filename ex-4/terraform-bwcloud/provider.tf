ffe4b09d9250f1e9ab097b0dd4e29ae1c4297f30
Christopher Hauser <christopher.hauser@uni-ulm.de>
2017-05-18 13:21:27 +0200

    remove my user from provider.tf

--- ex-4/terraform-bwcloud/provider.tf
+++ ex-4/terraform-bwcloud/provider.tf
@@ -1,7 +1,6 @@
-//variable "password" {}
 provider "openstack" {
-    user_name   = "ehx27@uni-ulm.de"
-    tenant_name = "Projekt_ehx27@uni-ulm.de"
+    user_name   = "xyz12@uni-ulm.de"
+    tenant_name = "Projekt_xyz12@uni-ulm.de"
     auth_url    = "https://bwcloud.ruf.uni-freiburg.de:5000/v2.0"
-    //password    = "${var.password}"
+    //password    = ""
 }
